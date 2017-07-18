f6012c37872f009bae5f9d5626af96e8072bdf4b
Samuel Obush <samuelobunge@yahoo.com>
2017-07-18 22:08:59 +0300

    Making the project accept relevant value

--- student.py
+++ student.py
@@ -5,10 +5,6 @@ user = registration.Register()
 connecting = sqlite.connection()
 
 
-
-
-#student.enterValues()
-
 connecting.connect()
 
 #introductory statements
@@ -27,12 +23,70 @@ if user.answer == 1:
             exit('Goodbye ...............................')
 
     if user.answer == 1:
-        connecting.insertUserValues(user.name, user.id, user.username, user.salt, user.hashed_password,
-                                user.age, user.address)
+        connecting.insertUserValues(user.name,user.id,user.username,user.role,user.salt,user.hashed_password,
+                                    user.age,user.address)
     #    connecting.conn.commit()
 else:
     user.login()
     connecting.authenticate(user.username,user.password)
+
+print("Enter your Role \n 1.Student \n 2.Lecture")
+
+role = int(input("Answer:"))
+
+if connecting.role == 1:
+    print("Which examination do you want to view \n 1.Year 1 semester 1 \n 2.Year 1 Semester 2\
+                                                 \n 3.Year 2 Semester 1 \n 4.Year 2 Semester 2 \
+                                                 \n 5.Year 3 Semester 1 \n 6.Year 3 Semester 2 \
+                                                 \n 7.Year 4 Semester 1 \n 8.Year 4 Semester 2")
+    answer = int(input("Answer:"))
+
+    if answer == 1:
+        print("######### Year 1 Semester 1 #########")
+    elif answer == 2:
+        print("######### Year 1 Semester 2 #########")
+    elif answer == 3:
+        print("######### Year 2 Semester 1 #########")
+    elif answer == 4:
+        print("######### Year 2 Semester 2 #########")
+    elif answer == 5:
+        print("######### Year 3 Semester 1 #########")
+    elif answer == 6:
+        print("######### Year 3 Semester 2 #########")
+    elif answer == 7:
+        print("######### Year 4 Semester 1 #########")
+    elif answer == 8:
+        print("######### Year 1 Semester 1 #########")
+    else:
+        print("You entered an invalid reponse")
+
+elif connecting.role == 2:
+
+        print("Which examination do you want to Enter \n 1.Year 1 semester 1 \n 2.Year 1 Semester 2\
+                                                     \n 3.Year 2 Semester 1 \n 4.Year 2 Semester 2 \
+                                                     \n 5.Year 3 Semester 1 \n 6.Year 3 Semester 2 \
+                                                     \n 7.Year 4 Semester 1 \n 8.Year 4 Semester 2")
+        answer = int(input("Answer:"))
+
+        if answer == 1:
+            print("######### Year 1 Semester 1 #########")
+        elif answer == 2:
+            print("######### Year 1 Semester 2 #########")
+        elif answer == 3:
+            print("######### Year 2 Semester 1 #########")
+        elif answer == 4:
+            print("######### Year 2 Semester 2 #########")
+        elif answer == 5:
+            print("######### Year 3 Semester 1 #########")
+        elif answer == 6:
+            print("######### Year 3 Semester 2 #########")
+        elif answer == 7:
+            print("######### Year 4 Semester 1 #########")
+        elif answer == 8:
+            print("######### Year 1 Semester 1 #########")
+        else:
+            print("You entered an invalid reponse")
+
 
 
 
