f20f3aa2037b5b89a92df455e43b46ef5b4070c2
Sobunge <samuelobunge@yahoo.com>
2017-09-27 12:39:14 +0300

    Learning gui edit

--- learningGui.py
+++ learningGui.py
@@ -1,75 +1,23 @@
-import tkinter
-from tkinter import *
+from tkinter import *;
 
-#def quit():
-  #  print('Exiting the system.......')
- #   import sys; sys.exit()
+class Window(Frame):
 
-#def login():
-  #  print('Checking credentials ....... \n')
+    def __init__(self,master = None):
+        Frame.__init__(self,master)
+        self.master = master
 
- #   username = usernameEntry.get()
-#    password = passwordEntry.get()
+        self.init_window()
 
-    #if username == 'sobunge' and password == 'merium@kema':
-   #     print('Login successful')
-  #  else:
- #       print('Login failed')
+    def init_window(self):
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
 
-#top = tkinter.Tk()
-#usernameLabel = Label(top,text='Username')
-#usernameEntry = Entry(top)
+        quitButton = Button(self, text="Quit")
+        quitButton.place(x=0,y=0)
 
-#passwordLabel = Label(top,text='Password')
-#passwordEntry = Entry(top,show='*')
+root = Tk()
+root.geometry("400x300")
 
-#button1 = Button(top,text = 'Login', command = login)
-#button2 = Button(top,text = 'Register')
-#button = Button(top,text ='Quit',command = quit)
+app = Window(root)
 
-#usernameLabel.pack()
-#usernameEntry.pack()
-
-#passwordLabel.pack()
-#passwordEntry.pack()
-
-#button2.pack(side = 'left',padx='10',pady='10')
-#button.pack(side='left')
-#button1.pack(side='left',padx='10',pady='10')
-
-#top.mainloop()
-
-def profil():
-    print('Users profil')
-
-def unitRegistration():
-    print('Register Units')
-
-def semesterResults():
-    print('Semester Results')
-
-def logout():
-    print('Loging out.....')
-
-def exit():
-    print('Exiting system.........')
-    import sys;sys.exit()
-
-top = tkinter.Tk()
-top.title("Users Dashboard")
-
-widget = Label(top,text='Users Dashboard',padx=5,pady=5)
-widget.pack(side=TOP)
-
-f = Frame(relief=RIDGE,borderwidth=5)
-widget1 = Label(f,text='User Name').pack()
-widget2 = Label(f,text='Users Role').pack()
-widget3 = Button(f,text='Profile',command=profil).pack(padx=10,pady=10)
-widget4 = Button(f,text='Unit Registration',command=unitRegistration).pack()
-widget5 = Button(f,text='Semester Results',command=semesterResults).pack(padx=10,pady=10)
-widget6 = Button(f,text='Logout',command=logout).pack()
-widget7 = Button(f,text='Exit',command=exit).pack(padx=10,pady=10)
-
-f.pack(side=LEFT)
-
-top.mainloop()
+root.mainloop()
